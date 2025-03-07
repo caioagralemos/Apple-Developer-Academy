a30fa0e4e70dab5a017d425b69df03ab0311e1b7
Caio <caioagralemos@live.com>
2025-03-06 23:44:45 -0300

    Abstract Factory

--- Pre-studies/Design Patterns/Factory.playground/Contents.swift
+++ Pre-studies/Design Patterns/Factory.playground/Contents.swift
@@ -64,4 +64,83 @@ func mainFactory() {
     let p2 = f.createPolar(rho: 7, theta: 1)
 }
 
-mainFactory()
+// mainFactory()
+
+
+/// Abstract Factory
+
+protocol HotDrink {
+    func consume()
+}
+
+class Tea: HotDrink {
+    func consume() {
+        print("It's four o'clock somewhere!")
+    }
+}
+
+class Coffee: HotDrink {
+    func consume() {
+        print("At last, some caffeine!")
+    }
+}
+
+protocol HotDrinkFactory {
+    init()
+    func prepare(amount:Int) -> HotDrink
+}
+
+class TeaFactory: HotDrinkFactory {
+    required init() {}
+    func prepare(amount: Int) -> HotDrink {
+        print("Put in tea bag, boil water, pour \(amount)ml and enjoy!")
+        return Tea()
+    }
+}
+
+class CoffeeFactory: HotDrinkFactory {
+    required init() {}
+    func prepare(amount: Int) -> HotDrink {
+        print("Grind some beans, boil water, pour \(amount)ml, add cream and enjoy!")
+        return Coffee()
+    }
+}
+
+class HotDrinkMachine {
+    enum AvailableDrink: String { // quebra o OCP
+        case coffee = "Coffee"
+        case tea = "Tea"
+        
+        static let all = [coffee, tea]
+    }
+    
+    internal var factories = [AvailableDrink: HotDrinkFactory]()
+    
+    internal var namedFactories = [(String, HotDrinkFactory)]()
+    
+    init() {
+        for drink in AvailableDrink.all {
+            let type: AnyClass? = NSClassFromString("Factory.\(drink.rawValue)Factory")
+            let factory = (type as! HotDrinkFactory.Type).init()
+            factories[drink] = factory
+            namedFactories.append((drink.rawValue, factory))
+        }
+    }
+    
+    func makeDrink() -> HotDrink {
+        print("Available drinks:")
+        for i in 0..<namedFactories.count {
+            print("\(i+1): \(namedFactories[i].0)")
+        }
+        let input = Int(readLine()!)!
+        return namedFactories[input].1.prepare(amount: 250)
+    }
+}
+
+func mainAbstractFactory() {
+    let machine = HotDrinkMachine()
+    print(machine.namedFactories.count)
+    let drink = machine.makeDrink()
+    drink.consume()
+}
+mainAbstractFactory()
